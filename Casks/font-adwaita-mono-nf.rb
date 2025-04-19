4b22e6c179a16d61300ded605256df046c2c4096
Jared <git@email.ktn.one>
2025-04-19 04:05:11 -0400

    Adjust URL

--- Casks/font-adwaita-mono-nf.rb
+++ Casks/font-adwaita-mono-nf.rb
@@ -2,7 +2,7 @@ cask 'font-adwaita-mono-nf' do
     version '48.2-nf'
     sha256 '5030faea10ca79a2ab7e5754ae12494d71238d65744f149a2cc056902deef6f3'
 
-    url 'https://github.com/ktnjared/Adwaita-Mono-Nerd-Font/releases/download/v#{version}/AdwaitaMonoNerdFont.zip'
+    url 'https://github.com/ktnjared/Adwaita-Mono-Nerd-Font/releases/download/#{version}/AdwaitaMonoNerdFont.zip'
     name 'Adwaita Mono Nerd Font'
     desc 'GNOMA Adwaita Font merged with complete Nerd Font glyphs'
     homepage 'https://github.com/ktnjared/Adwaita-Mono-Nerd-Font/'
