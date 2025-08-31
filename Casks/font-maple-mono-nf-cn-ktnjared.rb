59515f5e97bd6cdde5bb67cf91375214d80219eb
Jared <github@relay.ktn.one>
2025-08-31 13:46:55 -0400

    Create font-maple-mono-nf-cn-ktnjared.rb
    Added cask to get the latest version of https://github.com/ktnjared/maple-font-jared

---
+++ Casks/font-maple-mono-nf-cn-ktnjared.rb
@@ -0,0 +1,11 @@
+cask "maple-font-jared" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/ktnjared/maple-font-jared/releases/latest/download/maple-font-jared.zip"
+  name "Maple Font Jared"
+  desc "Custom Maple Mono font variant by ktnjared"
+  homepage "https://github.com/ktnjared/maple-font-jared"
+
+  font "maple-font-jared.ttf"
+end
