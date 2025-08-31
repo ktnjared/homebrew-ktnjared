2c6c4f1af5384b4090988742ad87ca9c929b49ef
Jared <github@relay.ktn.one>
2025-08-31 13:57:26 -0400

    Update font-maple-mono-nf-cn-ktnjared.rb
    Updated files and URLs

--- Casks/font-maple-mono-nf-cn-ktnjared.rb
+++ Casks/font-maple-mono-nf-cn-ktnjared.rb
@@ -2,10 +2,25 @@ cask "font-maple-mono-nf-cn-ktnjared" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/ktnjared/maple-font-jared/releases/latest/download/maple-font-jared.zip"
-  name "Maple Font Jared"
-  desc "Custom Maple Mono font variant by ktnjared"
-  homepage "https://github.com/ktnjared/maple-font-jared"
+  url "https://github.com/ktnjared/font-maple-mono-nf-cn-ktnjared/releases/latest/download/MapleMono-NF-CN"
+  name "Maple Mono (ktnjared remix)"
+  desc "Maple Mono font variant by ktnjared. Includes Nerd Font (-NF) and Chinese/Japanese glyphs (-CN)"
+  homepage "https://github.com/ktnjared/font-maple-mono-nf-cn-ktnjared"
 
-  font "maple-font-jared.ttf"
+  font "MapleMono-NFMono-CN-Bold.ttf"
+  font "MapleMono-NFMono-CN-BoldItalic.ttf"
+  font "MapleMono-NFMono-CN-ExtraBold.ttf"
+  font "MapleMono-NFMono-CN-ExtraBoldItalic.ttf"
+  font "MapleMono-NFMono-CN-ExtraLight.ttf"
+  font "MapleMono-NFMono-CN-ExtraLightItalic.ttf"
+  font "MapleMono-NFMono-CN-Italic.ttf"
+  font "MapleMono-NFMono-CN-Light.ttf"
+  font "MapleMono-NFMono-CN-LightItalic.ttf"
+  font "MapleMono-NFMono-CN-Medium.ttf"
+  font "MapleMono-NFMono-CN-MediumItalic.ttf"
+  font "MapleMono-NFMono-CN-Regular.ttf"
+  font "MapleMono-NFMono-CN-SemiBold.ttf"
+  font "MapleMono-NFMono-CN-SemiBoldItalic.ttf"
+  font "MapleMono-NFMono-CN-Thin.ttf"
+  font "MapleMono-NFMono-CN-ThinItalic.ttf"
 end
