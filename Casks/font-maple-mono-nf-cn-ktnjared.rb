845b91c50a59fdf17b1867c4e10e0ca3c6486616
Jared <github@relay.ktn.one>
2025-09-07 11:51:02 -0400

    Update font-maple-mono-nf-cn-ktnjared.rb
    Manually updated to latest release

--- Casks/font-maple-mono-nf-cn-ktnjared.rb
+++ Casks/font-maple-mono-nf-cn-ktnjared.rb
@@ -1,10 +1,11 @@
 cask "font-maple-mono-nf-cn-ktnjared" do
-  version :latest
-  sha256 :no_check
+  version "1757259363"
+  sha256 :no_check # You can replace this with the actual SHA256 if you want to lock it down
 
-  url "https://github.com/ktnjared/font-maple-mono-nf-cn-ktnjared/releases/latest/download/MapleMono-NF-CN.zip"
-  name "Maple Mono (ktnjared remix)"
-  desc "Maple Mono font variant by ktnjared. Includes Nerd Font (-NF) and Chinese/Japanese glyphs (-CN)"
+  url "https://github.com/ktnjared/font-maple-mono-nf-cn-ktnjared/releases/download/v#{version}/MapleMono-NF-CN-ktnjared-v#{version}.zip",
+      verified: "github.com/ktnjared/font-maple-mono-nf-cn-ktnjared/"
+  name "Maple Mono NF CN (ktnjared)"
+  desc "Customized Maple Mono font with Nerd Font and Simplified Chinese support"
   homepage "https://github.com/ktnjared/font-maple-mono-nf-cn-ktnjared"
 
   font "MapleMono-NFMono-CN-Bold.ttf"
@@ -23,4 +24,10 @@ cask "font-maple-mono-nf-cn-ktnjared" do
   font "MapleMono-NFMono-CN-SemiBoldItalic.ttf"
   font "MapleMono-NFMono-CN-Thin.ttf"
   font "MapleMono-NFMono-CN-ThinItalic.ttf"
+
+  caveats <<~EOS
+    This font includes Nerd Font glyphs and Simplified Chinese support.
+    Installed to ~/Library/Fonts and available system-wide.
+  EOS
+end
 end
