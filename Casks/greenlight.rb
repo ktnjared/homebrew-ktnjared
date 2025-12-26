5f21d7fefe03f73af21fab07ad33ce1afa80acd7
ktnjared <github.com@relay.ktn.one>
2025-12-26 14:23:16 -0500

    greenlight 2.4.1

--- Casks/greenlight.rb
+++ Casks/greenlight.rb
@@ -1,8 +1,8 @@
 cask "greenlight" do
-  version :latest
-  sha256 :no_check
+    version "2.4.1"
+    sha256 "7c2716ecae1ea0fbb9043e480de4c66f06feb449504fec59e308e21dbe8bb914"
 
-  url "https://github.com/unknownskl/greenlight/releases/latest/download/Greenlight-universal.dmg",
+    url "https://github.com/unknownskl/greenlight/releases/download/v#{version}/Greenlight-#{version}-universal.dmg",
       verified: "github.com/unknownskl/greenlight/"
   name "Greenlight"
   desc "Open-source client for xCloud and Xbox home streaming"
